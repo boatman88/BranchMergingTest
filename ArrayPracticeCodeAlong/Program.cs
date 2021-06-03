efa578165fefa1c0a6784012bb2ab804d0dc07b2
Boatman88 <seanboatman88@gmail.com>
2021-06-03 11:30:00 -0400

    addingtest

---
+++ ArrayPracticeCodeAlong/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ArrayPracticeCodeAlong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 15, 23, 19, 85, 97 };
+
+            foreach(int num in nums)
+            {
+                if(num % 5 == 0)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}            //Search Algorithm - this would be a linear search specifically
+/*Say we have this list of numbers:
+10, 15, 23, 19, 85, 97
+How would you write a loop that searches that array and prints every number disivible by 5*/
